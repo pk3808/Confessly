4084a8573d39af040f9bc964d45823e595f876b7
pk3808 <pk38p>
2025-01-18 23:52:38 +0530

    tsfix

--- app/pages/intro/page.tsx
+++ app/pages/intro/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import "./intro.css";
-import { motion, AnimatePresence } from "framer-motion";
 
 const IntroPage = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
